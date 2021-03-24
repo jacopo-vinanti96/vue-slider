29206452796a9edf4c34dbcba82caeef033d44b2
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-24 15:43:28 +0100

    Aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+const img1 = 'img/img1.png',
+      img2 = 'img/img2.png';
+
 const app = new Vue ({
   el: '#root',
   data: {
     title: 'Big title!',
-    custClass: 'blue'
+    custClass: 'blue',
+    image: img1
+  },
+  methods: {
+    enterFunc: function () {
+      this.image = img2;
+    },
+    leaveFunc: function () {
+      this.image = img1;
+    }
   }
 
 })
