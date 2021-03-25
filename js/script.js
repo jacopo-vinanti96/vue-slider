546e8b69ddb74c1f38fcab17fd86af1fe3bf4927
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-25 16:03:51 +0100

    Struttura Slider

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
-const img1 = 'img/img1.png',
-      img2 = 'img/img2.png';
+const img1 = {
+              url: 'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/10/75/fe/87/photo0jpg.jpg?w=1000&h=600&s=1',
+              alt: 'Argentario'
+            },
+      img2 = {
+              url: 'https://www.controradio.it/wp-content/uploads/2020/01/Saturnia.jpg',
+              alt: 'Saturnia'
+            },
+      img3 = {
+              url: 'https://irintronauti.altervista.org/wp-content/uploads/2016/07/20160710_144459.jpg',
+              alt: 'Cascata del Diborrato'
+            },
+      img4 = {
+              url: 'https://media-cdn.tripadvisor.com/media/photo-c/2560x500/09/67/ef/90/cascata-di-moraduccio.jpg',
+              alt: 'Cascata di Moraduccio'
+            };
 
 const app = new Vue ({
   el: '#root',
   data: {
-    title: 'Big title!',
-    custClass: 'blue',
-    image: img1
+    images: [
+      img1,
+      img2,
+      img3,
+      img4
+    ],
+    imgIndex: 2
   },
   methods: {
     enterFunc: function () {
